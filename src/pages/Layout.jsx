5dcc7a14d5b17aaf2cc45905950129179100b96b
Yeboka <89529214+Yeboka@users.noreply.github.com>
2024-01-24 17:48:34 +0600

    Initial commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,10 @@
+
+const Layout = () => {
+    return (
+        <div className="w-[100vw] h-[100vh] text-blue-500 flex items-center justify-center">
+            Hello World
+        </div>
+    );
+};
+
+export default Layout;
