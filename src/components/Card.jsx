11aba3b6e4eff6adf54de2c8b911f634051835b4
Yeboka <89529214+Yeboka@users.noreply.github.com>
2024-01-25 20:31:16 +0600

    add images(not fully) and fix box page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,15 +6,22 @@ function Card({box}) {
 
     return (
         // eslint-disable-next-line react/prop-types
-        <Link to={`/boxes/${box.name}`} className="max-w-sm w-full mr-4 mb-4 border border-[#E98074] rounded-lg shadow bg-[#EAE7DC]">
+        <Link to={`/boxes/${box.name}`}
+              className="max-w-sm w-full mr-4 mb-4 border border-[#E98074] rounded-lg shadow bg-[#EAE7DC]">
 
-            <img className="rounded-t-lg" src={imageUrl} alt=""/>
+            {
+                box.img_url ?
+                <img className="object-cover rounded-t-lg w-full h-2/3"
+                     src={window.location.origin + `/images/${box.img_url}.jpg`} alt=":/"/> :
+                <></>
+            }
 
-            <div className="p-5">
+
+            <div className={box.img_url ? "h-1/3 px-5 flex flex-col justify-center " : "p-5 flex flex-col justify-center "}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#E85A4F]">
                     {box.name}
                 </h5>
-                <p className={box.description ? "mb-3" : "" + `font-normal text-gray-700 dark:text-gray-400`}>
+                <p className={box.description ? "" : "" + `font-normal text-gray-700 dark:text-gray-400`}>
                     {box.description}
                 </p>
             </div>
--- src/pages/Box.jsx
+++ src/pages/Box.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {useParams} from 'react-router-dom';
 import boxes from '../../boxes.json'
 
@@ -9,31 +8,33 @@ function Box() {
     const box = boxes.filter((box) => (box.name === params.name))[0];
 
     return (
-        <div className="flex flex-col items-center m-3 bg-[#EAE7DC] border border-[#E98074] rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                 src="/docs/images/blog/image-4.jpg" alt="" />
-            <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#E85A4F]">
-                    {box.name}
-                </h5>
-                <p className="mb-3 font-normal text-gray-700">
-                    {box.description}
-                </p>
-                <form className="space-y-6" action="#">
-                    <h5 className="text-xl font-medium text-[#E85A4F]">Сообщите нам если бокс заполнен!</h5>
-                    <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#E85A4F] ">
-                            Введите номер или почту
-                        </label>
-                        <input type="email" name="email" id="email"
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                               placeholder="name@company.com" required/>
-                    </div>
-                    <button type="submit"
-                            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Сообщить о заполненности
-                    </button>
-                </form>
+        <div className="flex flex-col m-3 items-center bg-[#EAE7DC] border border-[#E98074] rounded-lg shadow md:flex-row">
+            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-1/2 md:rounded-none md:rounded-s-lg"
+                 src={window.location.origin + `/images/${box.img_url}.jpg`} alt="" />
+            <div className={"flex w-full justify-center"}>
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#E85A4F]">
+                        {box.name}
+                    </h5>
+                    <p className="mb-6 font-normal text-gray-700">
+                        {box.description}
+                    </p>
+                    <form className="space-y-6" action="#">
+                        <h5 className="text-xl mb-2 font-medium text-[#E85A4F]">Сообщите нам если бокс заполнен!</h5>
+                        <div>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#E85A4F] ">
+                                Введите номер или почту
+                            </label>
+                            <input type="email" name="email" id="email"
+                                   className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                   placeholder="name@company.com" required/>
+                        </div>
+                        <button type="submit"
+                                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            Сообщить о заполненности
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
 );
